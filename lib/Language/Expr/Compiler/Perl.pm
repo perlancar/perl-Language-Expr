85d3bc82f169d18c35694d236c1ae4f92925b2b6
Steven Haryanto (on Asus EEEPC) <stevenharyanto@gmail.com>
2010-06-14 14:56:59 +0700

    implement compiler: pair, or/xor, and, bit or/xor, bit and, comparison3, comparison

--- lib/Language/Expr/Compiler/Perl.pm
+++ lib/Language/Expr/Compiler/Perl.pm
@@ -14,95 +14,119 @@ use List::Util 'reduce';
 sub rule_pair {
     my ($self, %args) = @_;
     my $match = $args{match};
-    [$match->{key}, $match->{value}];
+    "$match->{key} => $match->{value}";
 }
 
 sub rule_or_xor {
     my ($self, %args) = @_;
     my $match = $args{match};
-    my $res = shift @{$match->{operand}};
+    my @res;
+    push @res, shift @{$match->{operand}};
     for my $term (@{$match->{operand}}) {
         my $op = shift @{$match->{op}//=[]};
-        if    ($op eq '||') { $res ||= $term          }
-        elsif ($op eq '//') { $res //= $term          }
-        elsif ($op eq '^^') { $res = ($res xor $term) }
+        if    ($op eq '||') { push @res, " || $term" }
+        elsif ($op eq '//') { push @res, " // $term" }
+        # add parenthesis because perl's xor precendence is low
+        elsif ($op eq '^^') { @res = ("(", @res, " xor $term)") }
     }
-    $res;
+    join "", grep {defined} @res;
 }
 
 sub rule_and {
     my ($self, %args) = @_;
     my $match = $args{match};
-    my $res = shift @{$match->{operand}};
+    my @res;
+    push @res, shift @{$match->{operand}};
     for my $term (@{$match->{operand}}) {
         my $op = shift @{$match->{op}//=[]};
-        if    ($op eq '&&') { $res &&= $term }
+        if    ($op eq '&&') { push @res, " && $term" }
     }
-    $res;
+    join "", grep {defined} @res;
 }
 
 sub rule_bit_or_xor {
     my ($self, %args) = @_;
     my $match = $args{match};
-    my $res = shift @{$match->{operand}};
+    my @res;
+    push @res, shift @{$match->{operand}};
     for my $term (@{$match->{operand}}) {
         my $op = shift @{$match->{op}//=[]};
-        if    ($op eq '|') { $res = $res+0 | $term }
-        elsif ($op eq '^') { $res = $res+0 ^ $term }
+        if    ($op eq '|') { push @res, " | $term" }
+        elsif ($op eq '^') { push @res, " ^ $term" }
     }
-    $res;
+    join "", grep {defined} @res;
 }
 
 sub rule_bit_and {
     my ($self, %args) = @_;
     my $match = $args{match};
-    my $res = shift @{$match->{operand}};
+    my @res;
+    push @res, shift @{$match->{operand}};
     for my $term (@{$match->{operand}}) {
         my $op = shift @{$match->{op}//=[]};
-        if    ($op eq '&') { $res = $res+0 & $term }
+        if    ($op eq '&') { push @res, " & $term" }
     }
-    $res;
+    join "", grep {defined} @res;
 }
 
-sub rule_equal {
+sub rule_comparison3 {
     my ($self, %args) = @_;
     my $match = $args{match};
-    my $res = shift @{$match->{operand}};
-    return $res unless @{$match->{operand}};
-    my $last_term = $res;
+    my @res;
+    push @res, shift @{$match->{operand}};
     for my $term (@{$match->{operand}}) {
         my $op = shift @{$match->{op}//=[]};
-        if    ($op eq '==' ) { return '' unless $res = ($last_term ==  $term) }
-        elsif ($op eq '!=' ) { return '' unless $res = ($last_term !=  $term) }
-        elsif ($op eq '<=>') { $res = ($last_term <=> $term) }
-        elsif ($op eq 'eq' ) { return '' unless $res = ($last_term eq  $term) }
-        elsif ($op eq 'ne' ) { return '' unless $res = ($last_term ne  $term) }
-        elsif ($op eq 'cmp') { $res = ($last_term cmp $term) }
-        $last_term = $term;
+        if    ($op eq '<=>') { push @res, " <=> $term" }
+        elsif ($op eq 'cmp') { push @res, " cmp $term" }
     }
-    $res;
+    join "", grep {defined} @res;
 }
 
-sub rule_less_greater {
+sub rule_comparison {
     my ($self, %args) = @_;
     my $match = $args{match};
-    my $res = shift @{$match->{operand}};
-    return $res unless @{$match->{operand}};
-    my $last_term = $res;
+    my @opds;
+    push @opds, shift @{$match->{operand}};
+    return '' unless defined $opds[0];
+    my @ops;
     for my $term (@{$match->{operand}}) {
+        push @opds, $term;
         my $op = shift @{$match->{op}//=[]};
-        die "Invalid syntax" unless defined($op);
-        if    ($op eq '<' ) { return '' unless ($last_term <  $term) }
-        elsif ($op eq '<=') { return '' unless ($last_term <= $term) }
-        elsif ($op eq '>' ) { return '' unless ($last_term >  $term) }
-        elsif ($op eq '>=') { return '' unless ($last_term >= $term) }
-        elsif ($op eq 'lt') { return '' unless ($last_term lt $term) }
-        elsif ($op eq 'gt') { return '' unless ($last_term gt $term) }
-        elsif ($op eq 'le') { return '' unless ($last_term le $term) }
-        elsif ($op eq 'ge') { return '' unless ($last_term ge $term) }
-        $last_term = $term;
+        if    ($op eq '==' ) { push @ops, '=='  }
+        elsif ($op eq '!=' ) { push @ops, '!='  }
+        elsif ($op eq 'eq' ) { push @ops, 'eq'  }
+        elsif ($op eq 'ne' ) { push @ops, 'ne'  }
+        elsif ($op eq 'ne' ) { push @ops, 'ne'  }
+        elsif ($op eq '<'  ) { push @ops, '<'   }
+        elsif ($op eq '<=' ) { push @ops, '<='  }
+        elsif ($op eq '>'  ) { push @ops, '>'   }
+        elsif ($op eq '>=' ) { push @ops, '>='  }
+        elsif ($op eq 'lt' ) { push @ops, 'lt'  }
+        elsif ($op eq 'le' ) { push @ops, 'le'  }
+        elsif ($op eq 'gt' ) { push @ops, 'gt'  }
+        elsif ($op eq 'ge' ) { push @ops, 'ge'  }
     }
-    1;
+    return $opds[0] unless @ops;
+    my @res;
+    my $lastopd;
+    my ($opd1, $opd2);
+    while (@ops) {
+        my $op = pop @ops;
+        if (defined($lastopd)) {
+            $opd2 = $lastopd;
+            $opd1 = pop @opds;
+        } else {
+            $opd2 = pop @opds;
+            $opd1 = pop @opds;
+        }
+        if (@res) {
+            @res = ("(($opd1 $op $opd2) ? ", @res, " : '')");
+        } else {
+            push @res, "($opd1 $op $opd2)";
+        }
+        $lastopd = $opd1;
+    }
+    join "", @res;
 }
 
 sub rule_bit_shift {
@@ -124,9 +148,11 @@ sub rule_add {
     push @res, shift @{$match->{operand}};
     for my $term (@{$match->{operand}}) {
         my $op = shift @{$match->{op}//=[]};
-        push @res, " ", $op, " ", $term;
+        if    ($op eq '.') { push @res, " . $term" }
+        if    ($op eq '+') { push @res, " + $term" }
+        if    ($op eq '-') { push @res, " - $term" }
     }
-    join "", @res;
+    join "", grep {defined} @res;
 }
 
 sub rule_mult {
