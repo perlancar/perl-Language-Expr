c528b3e67abe59b4651f695627d993fced767928
Steven Haryanto <stevenharyanto@gmail.com>
2010-06-17 13:25:09 +0700

    some docs

--- lib/Language/Expr.pm
+++ lib/Language/Expr.pm
@@ -150,11 +150,12 @@ sub enum_vars {
 When first adding expression support to L<Data::Schema>, I want a
 language that is simple enough so I can easily convert it to Perl,
 PHP, JavaScript, and others. I do not need a fully-fledged programming
-language (in fact, Expr is not even Turing-complete, it does not
-support assignment or loops). Instead, I just need some basic stuffs
-like mathematical/string/logical operators, arrays, hashes, and
-functions. This language will mostly be used inside templates and
-schemas.
+languag. In fact, Expr is not even Turing-complete, it does not
+support assignment or loops. Nor does it allow function definition
+(though it allows anonymous function in grep/map/usort). Instead, I
+just need some basic stuffs like mathematical/string/logical
+operators, arrays, hashes, functions, map/grep/usort. This language
+will mostly be used inside templates and schemas.
 
 =head2 Why don't you use Language::Farnsworth, or Math::Expression, or Math::Expression::Evaluator, or $FOO?
 
